565ffaada8a215f58ec74a4db78ddfa33e87a80d
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-08-06 09:03:05 +0000

    hif

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,12 @@ class BasicAuth(Auth):
     """
     Basic Authentication Class
     """
-    ...
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
