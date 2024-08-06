934ab67f0ab2cb459f1dedb3baec8cf8df033b2f
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-08-06 08:20:27 +0000

    do

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -35,7 +35,12 @@ class Auth():
         """
         returns None - request
         """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
