bf87a9ff4997ed73f9cdc6f342385b0795b9a21a
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-08-06 07:23:17 +0000

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,10 +12,19 @@ class Auth():
     Authorization Class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        eturns False - path
+        """
         return False
         
     def authorization_header(self, request=None) -> str:
+        """
+        returns None - request
+        """
         return None
     
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns None - request
+        """
         return None
