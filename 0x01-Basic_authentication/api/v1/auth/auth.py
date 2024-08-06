37760dc0859d361150005c9039a9413575dd3506
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-08-06 07:53:14 +0000

    rout

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,7 +5,8 @@ the API authentication
 """
 from flask import request
 from typing import List, TypeVar
-
+import os
+import fnmatch
 
 class Auth():
     """
@@ -15,7 +16,17 @@ class Auth():
         """
         eturns False - path
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
