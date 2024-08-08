f5e3df171205dfcbee6afa0ccf064dce97a35598
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-08-08 07:26:32 +0000

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,12 +8,13 @@ from typing import TypeVar
 from models.user import User
 import uuid
 
+
 class SessionAuth(Auth):
     """
     session
     """
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """
         session id
@@ -23,7 +24,7 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
         return user id
@@ -32,3 +33,16 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """
+        current user
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        user = User.get(user_id)
+        return user
