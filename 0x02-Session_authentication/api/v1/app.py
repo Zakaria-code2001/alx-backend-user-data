d18419833610b2c7b80f5cd76a9f80e88642c841
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-08-08 06:59:41 +0000

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,9 +63,10 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
-
+    if auth.session_cookie(request) is None:
+        abort(401)
     current_user = auth.current_user(request)
     if current_user is None:
         abort(403)
