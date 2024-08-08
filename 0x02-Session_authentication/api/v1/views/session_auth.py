4870e335278ad9a69c41ed01d074b771b65298e8
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-08-08 08:21:38 +0000

    hey

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,34 +1,30 @@
-#!/usr/bin/env python3
-""" Authentication views
-"""
 import os
-from flask import jsonify, request
+from typing import Tuple
+
+from flask import abort, jsonify, request
+
+from api.v1.app import auth
 from api.v1.views import app_views
 from models.user import User
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def auth_session():
+def session_login() -> Tuple[str, int]:
     """
-    Handle user login
-    Return:
-        dictionary representation of user if found else error message
+    Login
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == '':
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
+    if not password:
         return jsonify({"error": "password missing"}), 400
-    users = User.search({"email": email})
-    if not users or users == []:
+    user = User.search({'email': email})
+    if not user:
         return jsonify({"error": "no user found for this email"}), 404
-    for user in users:
-        if user.is_valid_password(password):
-            from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            resp = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
-            resp.set_cookie(session_name, session_id)
-            return resp
-    return jsonify({"error": "wrong password"}), 401
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    session_id = auth.create_session(getattr(user[0], 'id'))
+    response = jsonify(user[0].to_json())
+    response.set_cookie(os.getenv("SESSION_NAME"), session_id)
+    return response
